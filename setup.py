c96ece2e344ab1d0b8c06a3b0c332e71e51a5343
Radoslaw Jurczak <radoslawjurczak95@gmail.com>
2022-12-21 21:39:07 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="npd_assignment",
+    version="0.1.0",
+    author="Radosław Jurczak",
+    author_email="radoslaw.jurczak@student.uw.edu.pl",
+    packages=["npd_assignment", "npd_assignment.test"],
+    license="LICENSE.txt",
+    description="Final assignment for Data Analysis in Python course @ MIM UW",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "pytest",
+    ],
+)
